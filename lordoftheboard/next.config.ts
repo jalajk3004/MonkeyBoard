a3cd8e30dea1a872feae1cd1561a03882ad81093
Jalajk3004 <jalajkumar23989@gmail.com>
2025-03-08 16:51:10 +0000

    turbo

--- lordoftheboard/next.config.ts
+++ lordoftheboard/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      treeShaking: true
+    }, 
+  },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
     return config;
